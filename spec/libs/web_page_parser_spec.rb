bbf3a097bc9431f24eb0bd86c7bd3cd59428ed7f
Felipe Mesquita <felipemesquita@darwin.(none)>
2009-07-09 22:48:57 -0300

    Writing a spec for the HTML parser.

--- spec/libs/web_page_parser_spec.rb
+++ spec/libs/web_page_parser_spec.rb
@@ -1,6 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe WebPageParser do
+  before(:each) do
+    @parser = WebPageParser.new(File.expand_path(File.dirname(__FILE__) + '/../fixtures/web_page_parser.html'))
+  end
 
+  it "should be able to parse the Barcas S.A. website" do
+    @parser.parse.should == available_schedules
+  end
+
+  def available_schedules
+    %w(6h 9h10 14h 18h 22h30).collect { |s| Time.parse(s) }
+  end
 end
 
--- spec/models/boarding_spec.rb
+++ spec/models/boarding_spec.rb
@@ -1,14 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Boarding do
-  before(:each) do
-    @valid_attributes = {
-      :day_id => 1,
-      :schedule => Time.now
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    Boarding.create!(@valid_attributes)
-  end
 end
+
